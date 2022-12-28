c3873a8e8e3e26e45b230dc996c31da9ec1173ed
antzol <antzol@yandex.ru>
2022-12-28 14:01:01 +0300

    Add changed DB architecture support in the TVVM server

--- TVVMServer/ui/mediaviewerwindow.cpp
+++ TVVMServer/ui/mediaviewerwindow.cpp
@@ -59,8 +59,31 @@ std::optional<RadioServiceWidget*> MediaViewerWindow::createRadioServiceWidget(u
         return std::nullopt;
     }
 
+
+    QVBoxLayout *cellLayout{nullptr};
+    QLayoutItem *item = gridLayout->itemAtPosition(row, column);
+    if (!item)
+    {
+        QWidget *cellWidget = new QWidget();
+        cellLayout = new QVBoxLayout();
+        cellLayout->addStretch(1);
+        cellWidget->setLayout(cellLayout);
+        gridLayout->addWidget(cellWidget, row, column);
+    }
+    else
+    {
+        QWidget *cellWidget = qobject_cast<QWidget*>(item->widget());
+        if (!cellWidget)
+        {
+            loggable.logMessage(objectName(), QtWarningMsg,
+                                "ERROR! Cell for radio widgets contains the widget that not QWidget.");
+            return std::nullopt;
+        }
+        cellLayout = static_cast<QVBoxLayout*>(cellWidget->layout());
+    }
+
     RadioServiceWidget *wgt = new RadioServiceWidget(serviceName);
-    gridLayout->addWidget(wgt, row, column);
+    cellLayout->addWidget(wgt);
 
     return wgt;
 }
